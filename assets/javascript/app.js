92a8b2f2c3b56cad158bdadb5ab205cecf1e34b3
Ryan Parks <rjp@dhcp-10-102-185-16.wireless.northwestern.private>
2018-01-09 18:30:22 -0600

    pseudocode outline plus RPS if statements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,19 @@
+var pOneSelection;
+var pTwoSelection;
 var pOneWins = 0;
+var pOneTies = 0;
+var pOneLosses = 0;
 var pTwoWins = 0;
+var pTwoTies = 0;
+var pTwoLosses = 0;
+var database = firebase.database();
+
+function nameForm() {
+    $(".name").html("<form id='name-form'><input type='text' id='name-input' placeholder='Name'><input id='add-name' type='submit' value='Submit'></form>");
+};
 
 function initialScreen() {
-    $(".name").html("<form id='name-form'><input type='text' id='name-input' placeholder='Name'><input id='add-name' type='submit' value='Submit'></form>");
+    nameForm();
     $("#player-one").html("<h4>Player 1</h4>");
     $("#player-two").html("<h4>Player 2</h4>");
 };
@@ -20,3 +31,60 @@ function rpsRecord() {
 initialScreen();
 rpsOptions();
 rpsRecord();
+
+database.ref().on("value", function (snapshot) {
+
+});
+
+//TAKE PLAYER NAME INPUT
+//store name entry as player one in firebase
+//on form submit:
+//if (no names in database) {store name as player one}
+//database.ref().set()
+//display form to allow second name
+//nameForm();
+//if (one name in database) {store name as player two}
+//remove form 
+//trigger game
+
+//GAME
+//run rpsOptions() for player one
+//run "other player is picking function" for player two
+//on click event, save player one choice to firebase
+//run "other player is picking function" for player one
+//display player one selection (image) to player one only
+//run rpsOptions() for player two
+//on click event, save player two choice to firebase
+//display player two selection (image) for player two
+
+
+if ((pOneSelection === "r") && (pTwoSelection === "s")) {
+    pOneWins++;
+    pTwoLosses++;
+} else if ((pOneSelection === "r") && (pTwoSelection === "p")) {
+    pOneLosses++;
+    pTwoWins++;
+} else if ((pOneSelection === "s") && (pTwoSelection === "r")) {
+    pOneLosses++;
+    pTwoWins++;
+} else if ((pOneSelection === "s") && (pTwoSelection === "p")) {
+    pOneWins++;
+    pTwoLosses++;
+} else if ((pOneSelection === "p") && (pTwoSelection === "r")) {
+    pOneWins++;
+    pTwoLosses++;
+} else if ((pOneSelection === "p") && (pTwoSelection === "s")) {
+    pOneWins++;
+    pTwoLosses++;
+} else if (pOneSelection === pTwoSelection) {
+    pOneTies++;
+    pTwoTies++;
+};
+//print wins, losses and ties to the screen
+//pause on result screen
+//
+//reset game
+
+//Player exit
+//on page exit, remove player from gameplay slot
+//run nameForm() to allow a new player to join.
